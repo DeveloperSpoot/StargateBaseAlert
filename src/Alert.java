fddee20028fb82f3bc72190a063396f9ea6b1aec
Nathaniel Rivera <minecraftsteve104@gmail.com>
2022-06-12 23:07:37 -0700

    Stargate Command, Desktop base alarm application. Code 9 w/ audio.
    Signed-off-by: Nathaniel Rivera <minecraftsteve104@gmail.com>

--- src/Alert.java
+++ src/Alert.java
@@ -1,5 +1,8 @@
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class Alert extends JFrame {
@@ -13,9 +16,10 @@ public class Alert extends JFrame {
         setTitle("Base Alert");
         setContentPane(panel1);
         setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setUndecorated(true);
+        CodeText.setText("CODE 9");
         setVisible(true);
-
         while (true) {
             Base.setVisible(false);
             Alert.setVisible(false);
@@ -24,30 +28,38 @@ public class Alert extends JFrame {
             try {
                 TimeUnit.MILLISECONDS.sleep(500);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
             Base.setVisible(true);
             try {
                 TimeUnit.MILLISECONDS.sleep(500);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
             Alert.setVisible(true);
             try {
                 TimeUnit.MILLISECONDS.sleep(500);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
             CODEAlert.setVisible(true);
             try {
                 TimeUnit.MILLISECONDS.sleep(1000);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File file = new File("src/SGC_Code9.wav");
+        AudioInputStream AIS = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(AIS);
+        clip.start();
+        clip.loop(10000);
        Alert myAlert = new Alert();
+
+
     }
 }
